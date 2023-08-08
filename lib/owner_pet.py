d2cede5c3ebaa0356583824f7092877563a260f0
Vaschell Mkenzie <mvaschell@gmail.com>
2023-08-07 20:07:43 -0400

    done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -12,14 +12,14 @@ class Pet:
     @property
     def pet_type(self):
         return self._pet_type
-
+        # gets a animal
     
     @pet_type.setter
     def pet_type(self, pet_type):
         if pet_type not in self.PET_TYPES:
             raise Exception
         self._pet_type = pet_type
-        # if the pettype isnt in the list then raise an exception, else the pettype is in the list
+        # if the pet type isnt in the list then raise an exception, else the pettype is in the list
 
 
 class Owner:
@@ -37,5 +37,6 @@ class Owner:
       pet.owner = self
 # if the pet isnt a isinstance of the Pet class raise a Exception, else the the pet is a isinstance of the Pet class so add a owner to that pet
 
-    def sort_pets_by_name(self):
-        return sorted(self.pets())
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+    # sorts though the list of objects calls a an
